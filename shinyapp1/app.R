47c8dc5d5efb4a7afb069211bd8ac915c830cc07
Tyson Wepprich <tyson.wepprich@gmail.com>
2018-03-31 10:49:19 -0700

    Shiny app mechanics work for test scenario of photothermograph simulation with run button

--- shinyapp1/app.R
+++ shinyapp1/app.R
@@ -12,164 +12,91 @@ library(ggridges)
 
 source('helpers.R')
 
+# prefix is used to define the two scenarios to simulate
+renderInputs <- function(prefix) {
+  wellPanel(
+    fluidRow(
+      column(6,
+             sliderInput(paste0(prefix, "_", "nsim"), "Number of simulations:", min = 1, max = 500, value = 100, step = 1),
+             sliderInput(paste0(prefix, "_", "year_range"), "Range of observations (in Years):", min = 1980, max = 2016, value = c(2005, 2010), step = 1),
+             numericInput(paste0(prefix, "_", "lat"), "Site latitude (in decimal units, limited to North America):", min = 6.6, max = 83.3, value = 44.56, step = .1),
+             numericInput(paste0(prefix, "_", "lon"), "Site longitude (in decimal units, limited to North America):", min = -178.2, max = -49, value = -123.26, step = .1),
+             sliderInput(paste0(prefix, "_", "dev_temp"), "Temperature range for development (in Celsius):", min = -10, max = 45.0, value = c(10.0, 37.0), step = 0.1),
+             sliderInput(paste0(prefix, "_", "cdl_mu"), "Critical photoperiod (hours of light):", min = 8, max = 24, value = 15.5, step = 0.1),
+             sliderInput(paste0(prefix, "_", "cdl_sd"), "Std. dev. critical photoperiod:", min = 0, max = 3, value = 0.5, step = 0.1),
+             sliderInput(paste0(prefix, "_", "lambda"), "Population growth rate between generations:", min = .5, max = 5, value = 2, step = 0.1)
+             
+      ),
+      column(6,
+             sliderInput(paste0(prefix, "_", "emerg_dd"), "Mean overwinter emergence (in degree-days):", min = 0.0, max = 2000, value = 100, step = 1),
+             sliderInput(paste0(prefix, "_", "emerg_dd_sd"), "Std. dev. overwinter emergence:", min = 0.0, max = 50, value = 15, step = 1),
+             sliderInput(paste0(prefix, "_", "gen_dd"), "Mean development time egg to teneral adult (in degree-days):", min = 100, max = 2000, value = 367, step = 1),
+             sliderInput(paste0(prefix, "_", "gen_dd_sd"), "Std. dev. development time egg to teneral adult:", min = 0.0, max = 50, value = 30, step = 1),
+             sliderInput(paste0(prefix, "_", "povip_dd"), "Mean pre-oviposition time (in degree-days):", min = 0, max = 500, value = 126, step = 1),
+             sliderInput(paste0(prefix, "_", "povip_dd_sd"), "Std. dev. pre-oviposition time:", min = 0.0, max = 50, value = 10, step = 1),
+             sliderInput(paste0(prefix, "_", "ovip_dd"), "Mean time until oviposition (in degree-days, Poisson distribution):", min = 0, max = 200, value = 30, step = 1)
+      )
+    ),
+    p(actionButton(paste0(prefix, "_", "recalc"),
+                   "Re-run simulation", icon("refresh"))
+    )
+    # TODO: reactive options for plots once simulation runs
+    # see conditionalPanel function
+  )
+} # close renderInputs
+
 
 # Define UI ----
 ui <- fluidPage(
   
-  # prefix is used to define the two scenarios to simulate
-  renderInputs <- function(prefix) {
-    wellPanel(
-      fluidRow(
-        column(6,
-               sliderInput(paste0(prefix, "_", "nsim"), "Number of simulations:", min = 1, max = 500, value = 100, step = 1),
-               sliderInput(paste0(prefix, "_", "year_range"), "Range of observations (in Years):", min = 1980, max = 2016, value = c(2005, 2010), step = 1),
-               numericInput(paste0(prefix, "_", "lat"), "Site latitude (in decimal units, limited to North America):", min = 6.6, max = 83.3, value = 44.56, step = .1),
-               numericInput(paste0(prefix, "_", "lon"), "Site longitude (in decimal units, limited to North America):", min = -178.2, max = -49, value = -123.26, step = .1),
-               sliderInput(paste0(prefix, "_", "dev_temp"), "Temperature range for development (in Celsius):", min = -10, max = 45.0, value = c(10.0, 37.0), step = 0.1),
-               sliderInput(paste0(prefix, "_", "cdl_mu"), "Critical photoperiod (hours of light):", min = 8, max = 24, value = 15.5, step = 0.1),
-               sliderInput(paste0(prefix, "_", "cdl_sd"), "Std. dev. critical photoperiod:", min = 0, max = 3, value = 0.5, step = 0.1),
-               sliderInput(paste0(prefix, "_", "lambda"), "Population growth rate between generations:", min = .5, max = 5, value = 2, step = 0.1)
-               
-        ),
-        column(6,
-               sliderInput(paste0(prefix, "_", "emerg_dd"), "Mean overwinter emergence (in degree-days):", min = 0.0, max = 2000, value = 100, step = 1),
-               sliderInput(paste0(prefix, "_", "emerg_dd_sd"), "Std. dev. overwinter emergence:", min = 0.0, max = 50, value = 15, step = 1),
-               sliderInput(paste0(prefix, "_", "gen_dd"), "Mean development time egg to teneral adult (in degree-days):", min = 100, max = 2000, value = 367, step = 1),
-               sliderInput(paste0(prefix, "_", "gen_dd_sd"), "Std. dev. development time egg to teneral adult:", min = 0.0, max = 50, value = 30, step = 1),
-               sliderInput(paste0(prefix, "_", "povip_dd"), "Mean pre-oviposition time (in degree-days):", min = 0, max = 500, value = 126, step = 1),
-               sliderInput(paste0(prefix, "_", "povip_dd_sd"), "Std. dev. pre-oviposition time:", min = 0.0, max = 50, value = 10, step = 1),
-               sliderInput(paste0(prefix, "_", "ovip_dd"), "Mean time until oviposition (in degree-days, Poisson distribution):", min = 0, max = 200, value = 30, step = 1)
-        )
-      ),
-      p(actionButton(paste0(prefix, "_", "recalc"),
-                     "Re-run simulation")
-      )
-    )
-  } # close renderInputs
-  
-  
+  # Define UI for application that plots random distributions
+  fluidPage(theme="simplex.min.css",
+            tags$style(type="text/css",
+                       "label {font-size: 12px;}",
+                       ".recalculating {opacity: 1.0;}"
+            ),
+            
+            # Application title
+            tags$h2("Development, voltinism and photoperiod-based diapause decisions simulated for biocontrol insects"),
+            p("An adaptation of the model from ",
+              tags$a(href="https://esajournals.onlinelibrary.wiley.com/doi/abs/10.1890/14-2071.1", "Grevstad & Coop 2015"),
+              "with individual variation in critical photoperiod and development requirements"),
+            hr(),
+            
+            # TODO: Add more instructions/explanations above steps for user input
+            
+            fluidRow(
+              column(6, tags$h3("Scenario A")),
+              column(6, tags$h3("Scenario B"))
+            ),
+            fluidRow(
+              column(6, renderInputs("a")),
+              column(6, renderInputs("b"))
+            ),
+            fluidRow(
+              column(6,
+                     plotOutput("a_distPlot", height = "600px")
+              ),
+              column(6,
+                     plotOutput("b_distPlot", height = "600px")
+              )
+            )
+  )
   
   
 ) # close ui fluidPage
 
+
 # TODO: 
-# If year/lat/lon modified, download daymet data and calc photoperiod = tempdata
-# If tempdata or ldt/udt modified, calc accum gdd = accum_gdd
-# If accum_gdd or nsim or traits modified AND action button = run simulations and output plots
 # If plot options modified (after simulations run), modify plot output only
 
-# Global functions
-paramNames <- c("start_capital", "annual_mean_return", "annual_ret_std_dev",
-                "annual_inflation", "annual_inf_std_dev", "monthly_withdrawals", "n_obs",
-                "n_sim")
-
-simulate_nav <- function(start_capital = 2000000, annual_mean_return = 5.0,
-                         annual_ret_std_dev = 7.0, annual_inflation = 2.5,
-                         annual_inf_std_dev = 1.5, monthly_withdrawals = 1000,
-                         n_obs = 20, n_sim = 200) {
-  #-------------------------------------
-  # Inputs
-  #-------------------------------------
-  
-  # Initial capital
-  start.capital = start_capital
-  
-  # Investment
-  annual.mean.return = annual_mean_return / 100
-  annual.ret.std.dev = annual_ret_std_dev / 100
-  
-  # Inflation
-  annual.inflation = annual_inflation / 100
-  annual.inf.std.dev = annual_inf_std_dev / 100
-  
-  # Withdrawals
-  monthly.withdrawals = monthly_withdrawals
-  
-  # Number of observations (in Years)
-  n.obs = n_obs
-  
-  # Number of simulations
-  n.sim = n_sim
-  
-  
-  #-------------------------------------
-  # Simulation
-  #-------------------------------------
-  
-  # number of months to simulate
-  n.obs = 12 * n.obs
-  
-  # monthly Investment and Inflation assumptions
-  monthly.mean.return = annual.mean.return / 12
-  monthly.ret.std.dev = annual.ret.std.dev / sqrt(12)
-  
-  monthly.inflation = annual.inflation / 12
-  monthly.inf.std.dev = annual.inf.std.dev / sqrt(12)
-  
-  # simulate Returns
-  monthly.invest.returns = matrix(0, n.obs, n.sim)
-  monthly.inflation.returns = matrix(0, n.obs, n.sim)
-  
-  monthly.invest.returns[] = rnorm(n.obs * n.sim, mean = monthly.mean.return, sd = monthly.ret.std.dev)
-  monthly.inflation.returns[] = rnorm(n.obs * n.sim, mean = monthly.inflation, sd = monthly.inf.std.dev)
-  
-  # simulate Withdrawals
-  nav = matrix(start.capital, n.obs + 1, n.sim)
-  for (j in 1:n.obs) {
-    nav[j + 1, ] = nav[j, ] * (1 + monthly.invest.returns[j, ] - monthly.inflation.returns[j, ]) - monthly.withdrawals
-  }
-  
-  # once nav is below 0 => run out of money
-  nav[ nav < 0 ] = NA
-  
-  # convert to millions
-  nav = nav / 1000000
-  
-  return(nav)
-}
-
-plot_nav <- function(nav) {
-  
-  layout(matrix(c(1,2,1,3),2,2))
-  
-  palette(c("black", "grey50", "grey30", "grey70", "#d9230f"))
-  
-  # plot all scenarios
-  matplot(nav,
-          type = 'l', lwd = 0.5, lty = 1, col = 1:5,
-          xlab = 'Months', ylab = 'Millions',
-          main = 'Projected Value of Initial Capital')
-  
-  # plot % of scenarios that are still paying
-  p.alive = 1 - rowSums(is.na(nav)) / ncol(nav)
-  
-  plot(100 * p.alive, las = 1, xlab = 'Months', ylab = 'Percentage Paying',
-       main = 'Percentage of Paying Scenarios', ylim=c(0,100))
-  grid()
-  
-  
-  last.period = nrow(nav)
-  
-  # plot distribution of final wealth
-  final.nav = nav[last.period, ]
-  final.nav = final.nav[!is.na(final.nav)]
-  
-  if(length(final.nav) ==  0) return()
-  
-  plot(density(final.nav, from=0, to=max(final.nav)), las = 1, xlab = 'Final Capital',
-       main = paste0('Distribution of Final Capital\n', 100 * p.alive[last.period], '% are still paying'))
-  grid()
-}
 
 
-
-
-
-
-
-
-
-
+temp_param_names <- c("year_range", "lat", "lon")
+gdd_param_names <- c("dev_temp")
+sim_param_names <- c("nsim", "cdl_mu", "cdl_sd", "lambda", "emerg_dd", 
+                     "emerg_dd_sd", "gen_dd", "gen_dd_sd", "povip_dd", 
+                     "povip_dd_sd", "ovip_dd")
 
 
 # Define server logic ----
@@ -179,26 +106,84 @@ plot_nav <- function(nav) {
 # Idea and original code by Pierre Chretien
 # Small updates by Michael Kapler
 #
-function(input, output, session) {
+server <- function(input, output, session) {
   
-  getParams <- function(prefix) {
-    input[[paste0(prefix, "_recalc")]]
-    
-    params <- lapply(paramNames, function(p) {
+  
+  get_temp_params <- function(prefix) {
+    params <- lapply(temp_param_names, function(p) {
       input[[paste0(prefix, "_", p)]]
     })
-    names(params) <- paramNames
+    names(params) <- temp_param_names
     params
   }
   
-  # Function that generates scenarios and computes NAV. The expression
-  # is wrapped in a call to reactive to indicate that:
-  #
-  #  1) It is "reactive" and therefore should be automatically
-  #     re-executed when inputs change
-  #
-  navA <- reactive(do.call(simulate_nav, getParams("a")))
-  navB <- reactive(do.call(simulate_nav, getParams("b")))
+  get_gdd_params <- function(prefix) {
+    params <- lapply(gdd_param_names, function(p) {
+      input[[paste0(prefix, "_", p)]]
+    })
+    names(params) <- gdd_param_names
+    params
+  }
+  
+  get_sim_params <- function(prefix) {
+    params <- lapply(sim_param_names, function(p) {
+      input[[paste0(prefix, "_", p)]]
+    })
+    names(params) <- sim_param_names
+    params
+  }
+  
+  # Delay simulation until action button pressed
+  # If accum_gdd or nsim or traits modified AND action button = run simulations and output plots
+  
+  # valuesA <- reactiveValues()
+  # 
+  # # Break into steps
+  # 
+  # # If year/lat/lon modified, download daymet data and calc photoperiod
+  # valuesA$temp_data <- reactive(do.call(get_temp_data, get_temp_params("a")))
+  # 
+  # # If temp data or ldt/udt modified, calc accum gdd = accum_gdd
+  # 
+  # valuesA$gdd_data <- reactive(do.call(get_gdd_data, c(valuesA()$temp_data, get_gdd_params("a"))))
+  # 
+  # 
+  # valuesA$resultsA <- reactive(do.call(run_sims, c(get_sim_params("a"), valuesA()$gdd_data)))
+  # 
+  # 
+  output$a_distPlot <- renderPlot({
+    if (input$a_recalc == 0)
+      return()
+    
+    isolate({ 
+      temp_a <- get_temp_data(userinput = get_temp_params("a"))
+      gdd_a <- get_gdd_data(temp_data = temp_a, userinput = get_gdd_params("a"))
+      results_a <- run_sims(pars = get_sim_params("a"), gdd = gdd_a)
+      
+      # # debug without reactive
+      # temp_a <- get_temp_data(userinput = list("year_range" = c(2005, 2010), "lat" = 45, "lon" = -100))
+      # gdd_a <- get_gdd_data(temp_data = temp_a, userinput = list("dev_temp" = c(10, 30)))
+      # results_a <- run_sims(pars = as.list(pars), gdd = gdd_a)
+      # 
+      
+      plot_sim(results = results_a, gdd = gdd_a) 
+      })
+  })
+  
+  
+  # simB <- eventReactive(input$b_recalc, {
+  #   do.call(simulate_nav, getParams("b"))
+  #   })
+  # 
+  # simA <- eventReactive(input$a_recalc, {
+  #   sim_params <- getParams("a")
+  #   
+  #   tempdata <- 
+  #   
+  #   
+  # })
+  
+  
   
   # Expression that plot NAV paths. The expression
   # is wrapped in a call to renderPlot to indicate that:
@@ -207,14 +192,10 @@ function(input, output, session) {
   #     re-executed when inputs change
   #  2) Its output type is a plot
   #
-  output$a_distPlot <- renderPlot({
-    plot_nav(navA())
-  })
-  output$b_distPlot <- renderPlot({
-    plot_nav(navB())
-  })
-  
+  # output$a_distPlot <- renderPlot({
+  #   plot_sim(simA())
 }
+
 
 
 # Run the app ----
--- shinyapp1/helpers.R
+++ shinyapp1/helpers.R
@@ -34,3 +34,216 @@ photoperiod <- function(lat, doy, p = 1.5){
       (cos(lat * pi / 180) * cos(phi))
   )
 }
+
+# Download Daymet data and calculate photoperiod
+get_temp_data <- function(userinput = NA){
+  
+  startyear <- userinput$year_range[1]
+  endyear <- userinput$year_range[2]
+  lat <- userinput$lat
+  lon <- userinput$lon
+  # Download weather data
+  temp <- download_daymet(site = "default", lat = lat, lon = lon,
+                          start = startyear, end = endyear, internal = TRUE,
+                          silent = TRUE, force = FALSE)
+  # Daylength for each unique day at site
+  uniq_photo <- temp$data %>% 
+    filter(yday < 350) %>% 
+    dplyr::select(yday) %>% 
+    distinct() %>% 
+    mutate(daylength = photoperiod(lat, yday))
+  
+  out <- temp$data %>% 
+    filter(yday < 350) %>% 
+    left_join(uniq_photo, by = "yday")
+  return(out)
+}
+
+get_gdd_data <- function(temp_data = NA, userinput = NA){
+  ldt <- userinput$dev_temp[1]
+  udt <- userinput$dev_temp[2]
+  
+  gdd <- temp_data %>% 
+    rowwise() %>% 
+    mutate(degday = TriDD(tmax..deg.c., tmin..deg.c., ldt, udt)) %>% 
+    ungroup() %>% 
+    group_by(year) %>% 
+    arrange(yday) %>% 
+    mutate(accumdegday = cumsum(degday))
+  return(gdd)
+}
+
+
+
+SimLifecycle <- function(pars, yr, gdd){
+  gdd <- gdd %>% filter(year == yr)
+  maxgdd <- max(gdd$accumdegday, na.rm = TRUE)
+  firstfrost <- min(gdd$accumdegday[which(gdd$tmin..deg.c. <= 0 & gdd$yday > 200)])
+  lastfrost <- max(gdd$accumdegday[which(gdd$tmin..deg.c. <= 0 & gdd$yday < 180)])
+  
+  dflist <- list()
+  
+  # Simulate overwinter emergence
+  emerg_dd <- rnorm(pars$nsim, mean = pars$emerg_dd, sd = pars$emerg_dd_sd)
+  gen_dd <- rnorm(pars$nsim, mean = pars$gen_dd, sd = pars$gen_dd_sd)
+  povip_dd <- rnorm(pars$nsim, mean = pars$povip_dd, sd = pars$povip_dd_sd)
+  ovip_dd <- rpois(pars$nsim, lambda = pars$ovip_dd)
+  genlength1 <- emerg_dd + gen_dd
+  genlength2 <- gen_dd + povip_dd + ovip_dd
+  df <- data.frame(index = seq_len(pars$nsim), sens_dd = emerg_dd, gen = 0, reproduce = TRUE) %>% 
+    mutate(rowid = row_number()) %>% 
+    group_by(rowid) %>% 
+    mutate(chooserow = which.max(sens_dd <= gdd$accumdegday),
+           accumdegday = gdd$accumdegday[chooserow],
+           yday = gdd$yday[chooserow],
+           daylength = gdd$daylength[chooserow],
+           cdl = NA,
+           diap_dd = genlength1[rowid],
+           repro_dd = genlength2[rowid],
+           fit_diapause_gdd = maxgdd >= (accumdegday + genlength1[rowid]),
+           fit_repro_gdd = maxgdd >= (accumdegday + genlength2[rowid]),
+           diapause_before_frost = firstfrost > (accumdegday + genlength1[rowid]),
+           emerge_after_frost = lastfrost < sens_dd) %>% 
+    ungroup() %>%
+    dplyr::select(-rowid)
+  dflist[[1]] <- df
+  
+  # Maximal potential voltinism without photoperiod
+  maxvolt <- vector(length = length(emerg_dd))
+  lastsensgdd <- vector(length = length(emerg_dd))
+  
+  for (i in seq_along(emerg_dd)){
+    maxvolt[i] <- length(seq(from = genlength1[i],
+                             to = maxgdd, 
+                             by = genlength2[i]))
+    lastsensgdd[i] <- max(seq(from = genlength1[i],
+                              to = maxgdd, 
+                              by = genlength2[i]))
+  }
+  
+  voltinism <- data.frame(index = seq_len(pars$nsim), emerg_dd = emerg_dd, gen_dd = gen_dd, povip_dd = povip_dd,
+                          ovip_dd = ovip_dd, maxvolt = maxvolt, maxgdd = maxgdd, firstfrost = firstfrost, lastfrost = lastfrost,
+                          lastsensgdd = lastsensgdd)
+  
+  
+  eggs <- ceiling(pars$lambda)
+  mortality <- (eggs - pars$lambda) / eggs
+  
+  SimExpand <- function(dfrow){
+    outdf <- dfrow[rep(seq_len(nrow(dfrow)), eggs), ]
+    outdf$eggs <- seq_len(eggs)
+    outdf$sens_dd <- Cond(outdf$gen[1] == 0, outdf$accumdegday + outdf$diap_dd,
+                          outdf$accumdegday + outdf$repro_dd)
+    outdf <- outdf %>% 
+      rowwise() %>% 
+      mutate(chooserow = which.max(sens_dd <= gdd$accumdegday),
+             accumdegday = ifelse(chooserow == 1, NA, gdd$accumdegday[chooserow]),
+             yday = ifelse(chooserow == 1, NA, gdd$yday[chooserow]),
+             daylength = ifelse(chooserow == 1, NA, gdd$daylength[chooserow]),
+             gen = gen + 1,
+             cdl = rnorm(1, mean = pars$cdl_mu, sd = pars$cdl_sd),
+             reproduce = daylength >= cdl,
+             fit_diapause_gdd = maxgdd >= (accumdegday + diap_dd),
+             fit_repro_gdd = maxgdd >= (accumdegday + repro_dd),
+             diapause_before_frost = firstfrost > (accumdegday + diap_dd),
+             emerge_after_frost = NA)
+    
+    return(outdf)
+  }
+  
+  while(length(which(df$reproduce == TRUE & df$fit_repro_gdd == TRUE)) > 0){
+    df <- df %>% 
+      ungroup() %>% 
+      filter(reproduce == TRUE & fit_repro_gdd == TRUE) %>% 
+      group_by(row_number()) %>% 
+      do(SimExpand(.)) %>% 
+      ungroup() %>% 
+      sample_frac(size = (1 - mortality), replace = FALSE)
+    dflist[[length(dflist)+1]] <- df
+  }
+  
+  simresults <- bind_rows(dflist) %>% 
+    left_join(voltinism, by = "index") %>% 
+    dplyr::select(-`row_number()`, -eggs, -chooserow)
+  
+  return(simresults)  
+}
+
+
+# run_sims <- function(nsim=NA, cdl_mu=NA, cdl_sd=NA, lambda=NA, emerg_dd=NA, 
+#                      emerg_dd_sd=NA, gen_dd=NA, gen_dd_sd=NA, povip_dd=NA, 
+#                      povip_dd_sd=NA, ovip_dd=NA, gdd = NA){
+run_sims <- function(pars = NA, gdd = NA){
+  # repackage parameters
+  # pars <- data.frame(nsim = nsim, cdl_mu = cdl_mu, cdl_sd = cdl_sd,
+  #                    lambda = lambda, emerg_dd = emerg_dd, emerg_dd_sd = emerg_dd_sd, 
+  #                    gen_dd = gen_dd, gen_dd_sd = gen_dd_sd, povip_dd = povip_dd, 
+  #                    povip_dd_sd = povip_dd_sd, ovip_dd = ovip_dd)
+  pars <- data.frame(nsim = pars$nsim, cdl_mu = pars$cdl_mu, cdl_sd = pars$cdl_sd,
+                     lambda = pars$lambda, emerg_dd = pars$emerg_dd, emerg_dd_sd = pars$emerg_dd_sd, 
+                     gen_dd = pars$gen_dd, gen_dd_sd = pars$gen_dd_sd, povip_dd = pars$povip_dd, 
+                     povip_dd_sd = pars$povip_dd_sd, ovip_dd = pars$ovip_dd)
+  
+  sims <- expand.grid(year = unique(gdd$year))
+  results <- sims %>% 
+    group_by(year) %>% 
+    do(SimLifecycle(pars, .$year, gdd))
+  return(results)
+}
+
+
+plot_sim <- function(results, gdd){
+  
+  # Marginal histograms ----
+  mean_cdl <- mean(results$cdl, na.rm = TRUE)
+  maxgdd <- max(gdd$accumdegday, na.rm = TRUE)
+  maxphoto <- max(c(max(gdd$daylength, na.rm = TRUE), max(results$cdl, na.rm = TRUE)))
+  minphoto <- min(gdd$daylength, na.rm = TRUE)
+  
+  cdl_hist <- results %>% 
+    ungroup() %>% 
+    dplyr::select(cdl) %>%
+    filter(complete.cases(.)) %>% 
+    mutate(breaks = cut(cdl, breaks=seq(min(cdl),max(cdl),length.out = 20), 
+                        labels=seq(min(cdl),max(cdl), length.out = 20)[-1], 
+                        include.lowest=TRUE)) %>% 
+    mutate(daylength = as.numeric(as.character(breaks))) %>%
+    group_by(daylength) %>% 
+    summarise(n = n()) %>% 
+    mutate(accumdegday = -50 + 50 * n/max(n))
+  
+  sens_hist <- results %>% 
+    ungroup() %>% 
+    dplyr::select(accumdegday) %>%
+    filter(complete.cases(.)) %>% 
+    mutate(breaks = cut(accumdegday, breaks = seq(min(accumdegday), max(accumdegday), length.out = 100),
+                        labels = seq(min(accumdegday), max(accumdegday), length.out = 100)[-1],
+                        include.lowest = TRUE)) %>% 
+    mutate(accumdegday = as.numeric(as.character(breaks))) %>% 
+    group_by(accumdegday) %>% 
+    summarise(n = n()) %>% 
+    mutate(daylength = minphoto - .5 + .5 * n/max(n))
+  
+  # Photothermograph ----
+  theme_set(theme_bw(base_size = 16)) 
+  
+  
+  plt <- ggplot(gdd, aes(x = accumdegday, y = daylength, group = as.factor(year), color = as.factor(year))) +
+    geom_line(size = 1) +
+    coord_cartesian(xlim = c(-50, maxgdd + 100), ylim = c(minphoto - .5, maxphoto), expand = FALSE) +
+    scale_color_viridis(discrete = TRUE, name = "Year", option = "D") +
+    guides(color = guide_legend(reverse=FALSE)) +
+    geom_segment(data=cdl_hist, size=2.5, show.legend=FALSE,
+                 aes(x=-50, xend=accumdegday, y=daylength, yend=daylength), inherit.aes = FALSE) +
+    geom_segment(data=sens_hist, size=2.5, show.legend=FALSE,
+                 aes(x=accumdegday, xend=accumdegday, y=minphoto - .5, yend=daylength), inherit.aes = FALSE) +
+    geom_hline(yintercept = mean_cdl, linetype = "dashed", size = 1, alpha = .5) +
+    # geom_rug(data = results, aes(x = accumdegday), color = "gray", alpha = .1) +
+    # geom_rug(data = results, aes(y = cdl), color = "gray", alpha = .1) +
+    ggtitle("Photothermographs with critical photoperiod\nand simulated sensitive stage emergence") +
+    xlab("Accumulated degree-days") +
+    ylab("Daylength (hours)")
+  plt
+  
+  
+}
